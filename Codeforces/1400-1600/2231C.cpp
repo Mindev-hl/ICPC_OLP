49b57ba39b6ca015dcbc8d3e776ea2422dcb562b
Mindev-hl <long.trinh.contact@gmail.com>
2026-06-08 08:23:51 +0700

    2231C

--- Codeforces/1400-1600/2231C.cpp
+++ Codeforces/1400-1600/2231C.cpp
@@ -13,20 +13,18 @@ void solve()
         cin >> a[i];
     }
 
-    // Mảng phẳng lưu trữ mọi trạng thái dưới dạng {giá trị, số bước}
     vector<pair<ll, ll>> all_states;
-    // Cấp phát trước bộ nhớ để tăng tốc (mỗi số max 70 bước)
     all_states.reserve(n * 70);
 
     for (int i = 0; i < n; i++)
     {
         ll x = a[i];
         int tmp = 0;
-        vector<ll> visited; // Thay thế set bằng vector siêu nhẹ
+        vector<ll> visited;
 
         while (true)
         {
-            // Kiểm tra xem x đã từng xuất hiện trong đường đi của chính nó chưa
+
             bool found = false;
             for (ll v : visited)
             {
@@ -37,12 +35,11 @@ void solve()
                 }
             }
             if (found)
-                break; // Kẹt vào chu kỳ -> ngắt vòng lặp
+                break;
 
             visited.push_back(x);
-            all_states.push_back({x, tmp}); // Ghi nhận dấu chân
+            all_states.push_back({x, tmp});
 
-            // Áp dụng luật biến đổi
             if (x % 2 == 0)
             {
                 x /= 2;
@@ -55,29 +52,25 @@ void solve()
         }
     }
 
-    // Sắp xếp lại toàn bộ trạng thái để gom các giá trị giống nhau lại gần nhau
     sort(all_states.begin(), all_states.end());
 
     ll min_ops = -1;
     int m = all_states.size();
 
-    // Quét mảng phẳng để tìm đáp án
     for (int i = 0; i < m;)
     {
         int j = i;
         ll current_val = all_states[i].first;
         ll current_sum = 0;
 
-        // Trượt con trỏ j để gộp tất cả các trạng thái có cùng giá trị
         while (j < m && all_states[j].first == current_val)
         {
             current_sum += all_states[j].second;
             j++;
         }
 
-        int freq = j - i; // Tần suất xuất hiện của giá trị này
+        int freq = j - i;
 
-        // Nếu giá trị này được chạm tới bởi cả n số ban đầu
         if (freq == n)
         {
             if (min_ops == -1 || current_sum < min_ops)
@@ -86,7 +79,7 @@ void solve()
             }
         }
 
-        i = j; // Nhảy cóc qua giá trị mới
+        i = j;
     }
 
     cout << min_ops << "\n";
@@ -105,5 +98,4 @@ int main()
             solve();
         }
     }
-    return 0;
 }
